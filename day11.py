19215807383f4a4a8cf6104a012dc629ee415d47
Gaven Dcosta <112816730+TeddyGaven@users.noreply.github.com>
2023-06-04 08:41:08 +0530

    Add files via upload

---
+++ day11.py
@@ -0,0 +1,14 @@
+#Leet Code
+
+#Power of Two
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+       power = 0
+       temp = 0
+       while temp <= n:
+           temp = math.pow(2, power)
+           if temp == n:
+               return True
+           power+=1
+       return False
