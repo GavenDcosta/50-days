35fe6dfc141bacc607de2dd0a0949a097104bb23
Gaven Dcosta <112816730+TeddyGaven@users.noreply.github.com>
2023-07-05 08:50:41 +0530

    Add files via upload

---
+++ day40.py
@@ -0,0 +1,12 @@
+#Leet Code
+
+# Search in Rotated Sorted Array
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        for i in range(len(nums)):
+            if nums[i]==target:
+                return nums.index(nums[i])
+                break
+        else:
+            return -1      
